c22b23f1605366c4d48109cd61eeb02fca60add0
SebastianBobes <bobes_sebastian03@yahoo.com>
2024-10-02 17:49:32 +0300

    finished login

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import authentication as auth
 app = Flask(__name__)
-
-
+app.secret_key = 'acwo2024'
 @app.route("/")
 def first_function():
     return render_template("index.html")
+
+@app.route("/loggedin")
+def loggedin_function():
+    username = session.get('username')
+    if username ==None:
+        return render_template("index.html")
+    return render_template('loggedin.html', username=username)
 
 
 @app.route("/quiz")
@@ -15,13 +21,21 @@ def second_function():
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
+    # if 'username' in session:
+    #     return redirect(url_for('index.html'))
     if request.method == 'POST':
         name = request.form["name"]
         password = request.form["Password"]
         if auth.check_password(name, password):
-            return redirect(url_for('first_function'))
+            session['username']=name
+            print(session['username'])
+            return redirect(url_for('loggedin_function'))
 
     return render_template("login.html")
+@app.route("/logout")
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('first_function'))
 
 
 if __name__ == '__main__':
